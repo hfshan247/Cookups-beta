2ea33b81340f60270578ccc1774b113cf6187cc9
hussain farooq <hussainfarooq@mysticbotss-imac.local>
2018-07-19 11:36:48 +0500

    Initial Commit

---
+++ Cookups/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Cookups
+//
+//  Created by hussain farooq on 19/07/2018.
+//  Copyright © 2018 hussain farooq. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CookupsTests/CookupsTests.swift
@@ -0,0 +1,36 @@
+//
+//  CookupsTests.swift
+//  CookupsTests
+//
+//  Created by hussain farooq on 19/07/2018.
+//  Copyright © 2018 hussain farooq. All rights reserved.
+//
+
+import XCTest
+@testable import Cookups
+
+class CookupsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
